9518c8d861ecc111d6990b834a3df0bb02d039c7
shubhamgohad221997 <shubhamgohad221997@gmail.com>
2022-10-21 11:10:26 +0530

    Updates

---
+++ precache-manifest.0376de25c06e48acb6556236c8aaba88.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9220bbf7f0a7046252a1ec18865ef465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9ef1dcdc4fcfe676f5",
+    "url": "/static/css/main.cabe3dac.chunk.css"
+  },
+  {
+    "revision": "04427ac5c3b4868a988e",
+    "url": "/static/js/2.5307a84e.chunk.js"
+  },
+  {
+    "revision": "1a17c67f4311437e33b522bcde67da07",
+    "url": "/static/js/2.5307a84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9ef1dcdc4fcfe676f5",
+    "url": "/static/js/main.ad3f5881.chunk.js"
+  },
+  {
+    "revision": "e39a942170c1a9b8ce0b",
+    "url": "/static/js/runtime-main.a4e17a6e.js"
+  },
+  {
+    "revision": "8da8fcc917315717f832103e5756c30a",
+    "url": "/static/media/Shubham1.8da8fcc9.png"
+  },
+  {
+    "revision": "1a06f44d34bdc6df1baecbaa123c3731",
+    "url": "/static/media/ShubhamGohad_Resume.1a06f44d.pdf"
+  },
+  {
+    "revision": "244831e08af81d1777a904803d799f95",
+    "url": "/static/media/back.244831e0.png"
+  },
+  {
+    "revision": "0fa0030b8b569371a09fba3330d29b89",
+    "url": "/static/media/back2.0fa0030b.png"
+  },
+  {
+    "revision": "16e529ce8960b753d1e73a3e337bb46d",
+    "url": "/static/media/back3.16e529ce.png"
+  },
+  {
+    "revision": "cfc539ea2dd54b0e8e96f67a11f14c18",
+    "url": "/static/media/circle.cfc539ea.png"
+  },
+  {
+    "revision": "05b023783c642086be1107b09515e79d",
+    "url": "/static/media/header_image-Recovered-Recovered.05b02378.png"
+  },
+  {
+    "revision": "aee8a0975c7a0313b301472f085bceb5",
+    "url": "/static/media/mytheresa.aee8a097.png"
+  },
+  {
+    "revision": "b763f740ddd91383ee34dbcb93d883db",
+    "url": "/static/media/nordstrom.b763f740.png"
+  },
+  {
+    "revision": "b76f9aa1c4d162417d9112b82e311e09",
+    "url": "/static/media/time.b76f9aa1.png"
+  },
+  {
+    "revision": "ca5a7a88f9e76cc6b5fae53f114f4e10",
+    "url": "/static/media/urbancompany.ca5a7a88.png"
+  }
+]);
